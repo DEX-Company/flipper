96a247a66cccb33f3e514e7df5a8e8a9e1c6d9ad
billbsing <bill.barman@dex.sg>
2019-07-23 14:47:37 +0800

    more docs

--- ocean_drop/sync.py
+++ ocean_drop/sync.py
@@ -19,6 +19,13 @@ logger = logging.getLogger('ocean_drop.sync')
 
 class Sync():
     def __init__(self, ocean, squid_agent):
+        """
+
+        Initialise the Sync object for loading sync information
+
+        :param ocean: Ocean objet that contains the information for the Ocean Network
+        :param squid_agent: Squid agent to get the asset listings registered.
+        """
         self._ocean = ocean
         self._squid_agent = squid_agent
         self._file_list = []
@@ -32,6 +39,19 @@ class Sync():
 
 
     def analyse(self, drop_path, drop_secret,  asset_tag):
+        """
+
+        Analyse and compare what is already available on the local folder and
+        what assets are registered.
+
+        :param str drop_path: Path of the files currently saved in the drop folder.
+        :param str drop_secret: Secret text that both uploader and downloader share,
+        so that we can make sure that both users will upload and download the same assets.
+
+        :param str asset_tag: tag used to search the correct assets
+
+        :return stats: Return the stats dict
+        """
         self._file_list = []
         total_size = 0
         self._file_list, total_size = read_file_list(drop_path, self._file_list, total_size)
@@ -56,39 +76,63 @@ class Sync():
         return self._stats
 
     def sync_lists(self):
+        """
+
+        Create a list of sync items that need to be uploaded and downloaded.
+
+        """
         self._sync_file_list = []
         self._download_list = []
         self._upload_list = []
         self._download_file_list = []
         self._upload_file_list = []
 
+        # scan through the registered assets
         for listing in self._listing_list:
             is_found = False
             for file_item in self._file_list:
                 if file_item['is_file'] and file_item['size'] > 0:
                     if self.is_listing_equal_to_file_item(listing, file_item['md5_hash']):
+                        # if both file in the folder and registered asset is the same
+                        # then add to the _sync_file_list
                         self._sync_file_list.append((listing, file_item))
                         is_found = True
                         break
             if not is_found:
+                # if the file in the folder is not found in the listing of registered
+                # assets then add to the list of assets that can be downloaded
                 self._download_list.append(listing)
                 remote_asset = listing.asset.get_asset_at_index(0)
                 filename = get_filename_from_metadata(listing.ddo.metadata['base'])
                 self._download_file_list.append(filename)
 
+        # now scan through all fo the local files
         for file_item in self._file_list:
             if file_item['is_file'] and file_item['size'] > 0:
                 is_found = False
                 for listing in self._listing_list:
                     if self.is_listing_equal_to_file_item(listing, file_item['md5_hash']):
+                        # found the file on disk and also as a registered asset
                         is_found = True
                         break
+                # now add the file to the to upload list, if not found as a registered asset
                 if not is_found:
                     self._upload_list.append(file_item)
                     self._upload_file_list.append(file_item['relative_filename'])
 
 
     def get_valid_listings(self, asset_tag, drop_secret):
+        """
+
+        Return a list of valid listings for registered assets.
+
+        :param str asset_tag: Text tag to search for a valid listing.
+        :param str drop_secret: Text secret to validate the listing with, so that
+        we know this has been registered by a valid uploader.
+
+        :return: a list of listing objects
+
+        """
         result = []
         search_filter = {
             'tags': [asset_tag]
@@ -100,6 +144,17 @@ class Sync():
         return result
 
     def is_listing_valid(self, listing, drop_secret):
+        """
+
+        Return True if the listing is a valid listing and has the same checksum
+        created by using the same drop_secret.
+
+        :Param object listing: The listing to check to see if it's valid.
+        :param str drop_secret: The shared secret to use in calculating the checksum.
+
+        :return: True if the listing is valid and created by the opposite uploader.
+
+        """
         data = listing.data
         if data and 'extra_data' in data:
             extra_data = data['extra_data']
@@ -110,6 +165,15 @@ class Sync():
         return False
 
     def is_listing_equal_to_file_item(self, listing, file_hash):
+        """
+        Check to see if the listing is the same as the saved file on disk.
+
+        :param object listing: Listing object to check.
+        :param str file_hash: Hash of the file that we currently have no disk.
+
+        :return: True if the hash on disk is the same as the hash saved in the listing.
+
+        """
         data = listing.data
         logger.debug(f'{data}')
         if data and 'extra_data' in data:
@@ -121,6 +185,12 @@ class Sync():
 
     @property
     def stats_to_text_list(self):
+        """
+
+        Return the stats as a text string
+
+        :return: Text string describing the sync stats.
+        """
         total_space_text = show_size_as_text(self._stats['total_size'])
         result = [
             f'Total files: {self._stats["file_count"]}',
--- ocean_drop/utils.py
+++ ocean_drop/utils.py
@@ -18,6 +18,18 @@ logger = logging.getLogger('ocean_drop.utils')
 BYTES_PER_KB = 1000
 
 def read_file_list(folder, file_list, total_size):
+    """
+
+    Read the file list ard return a list of files as a dict. Each file will have
+    it's timestamp, name , full path, relative path set
+
+    :param str folder: folder to search for files.
+    :param list file_list: Current file list to append files too.
+    :param int total_size: Current total size of the files read.
+
+    :return: file_list, total_size updated by the search of a folder
+
+    """
     logger.debug(f'Scanning folder {folder}')
     for root, dirs, files in os.walk(folder):
         if len(files) == 0 and len(dirs) == 0:
@@ -48,6 +60,17 @@ def read_file_list(folder, file_list, total_size):
     return (file_list, total_size)
 
 def get_relative_path(base_folder, folder, filename):
+    """
+
+    Return the relative path based on a base folder.
+
+    :param str base_folder: base folder calculate the relative path from.
+    :param str folder: The folder where the file is saved.
+    :param str filnemane: Filename of the file
+
+    :return: Relative path of the filename with respect to the base_folder.
+
+    """
     base_folder_abs_path = os.path.abspath(base_folder)
     folder_abs_path = os.path.abspath(folder)
     relative_path = folder_abs_path[len(base_folder_abs_path):]
@@ -56,6 +79,14 @@ def get_relative_path(base_folder, folder, filename):
     return relative_filanme
 
 def hash_file(filename):
+    """
+
+    Return the hash of a file contents.
+
+    :return: string hash of the file contents
+
+    """
+
     hash_md5 = hashlib.md5()
     with open(filename, "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
@@ -63,6 +94,13 @@ def hash_file(filename):
     return hash_md5.hexdigest()
 
 def show_size_as_text(size):
+    """
+
+    Show the size in text readable form.
+
+    :return: the size in bytes, kb, mb e.t.c depending on the quantit of size.
+    
+    """
     result = f'{size} Bytes'
     items = [
         (BYTES_PER_KB, 'KB'),
